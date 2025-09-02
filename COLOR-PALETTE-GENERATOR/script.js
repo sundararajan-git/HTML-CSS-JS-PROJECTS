cf1a129757e29dd3ad7258ee3685cf21da00e512
sundararajan-git <sundararajanselvarasu@gmail.com>
2025-09-02 15:13:50 +0530

    color plate gen done

--- COLOR-PALETTE-GENERATOR/script.js
+++ COLOR-PALETTE-GENERATOR/script.js
@@ -15,19 +15,30 @@ const genCodes = () => {
   hexCodes.push(hexCode);
 };
 
+function codeCopy(e) {
+  const code = '#' + JSON.parse(e.dataset.code)
+  navigator.clipboard.writeText(code).then(() => {
+    alert(`Copied -  ${code} `)
+  }).catch(() => {
+    alert(`Somthing wrong !`)
+  })
+}
+
 const generateBtnHandler = () => {
   for (let i = 0; i < 25; i++) {
     genCodes();
   }
   let templates = "";
   hexCodes.map((code) => {
-    templates += `<div>
-                    <div id="color" style="background-color: #${code}; width: 100px; height: 100px;"></div>
-                    <p id="code">#${code}</p>
-                  </div>`;
+    templates += `<div class="color-card" data-code ='${JSON.stringify(code)}' onClick="codeCopy(this)">
+      <div id = "color" class= "color-box" style = "background-color: #${code};" ></div>
+    <p id="code" class="hex-code">#${code}</p>
+        </div> `;
   });
   container.innerHTML = templates;
 };
+
+
 
 window.addEventListener("DOMContentLoaded", generateBtnHandler);
 generate.addEventListener("click", generateBtnHandler);
