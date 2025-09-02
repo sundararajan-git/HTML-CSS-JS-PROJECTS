4f1d805362604a645995cada655f59662af51cc9
sundararajan-git <sundararajan-git@users.noreply.github.com>
2025-09-02 11:07:06 +0000

    deploy: 49bf6da6204e010e7996e010f83983709dcf7a51

--- DRAG-AND-DROP/script.js
+++ DRAG-AND-DROP/script.js
@@ -1,15 +1,41 @@
-const cards = document.querySelectorAll(".card");
-const items = document.querySelectorAll(".item");
+const items = document.querySelectorAll(".item")
+const cards = document.querySelectorAll(".card")
 
-for (let card of cards) {
-  card.addEventListener("dragstart", dragstart);
-  card.addEventListener("dragend", dragEnd);
+
+// items func
+function dragStart(e) {
+  e.dataTransfer.setData("text/plain", e.target.id)
+  setTimeout(() => {
+    e.target.style.display = "none"
+  }, 0)
 }
 
-const dragstart = (e) => {
-  e.dataTransfer.setData("text/plain", this.id);
-};
+function dragEnd(e) {
+  e.target.style.display = "block"
+}
 
-const dragEnd = () => {
-  console.log("deog end");
-};
+// items
+items.forEach((item) => {
+  item.addEventListener("dragstart", dragStart)
+  item.addEventListener("dragend", dragEnd)
+})
+
+
+function dragOver(e) {
+  e.preventDefault()
+  e.currentTarget.classList.add("over")
+}
+
+function drag(e) {
+  e.preventDefault()
+  const id = e.dataTransfer.getData("text/plain")
+  const item = document.getElementById(id)
+  e.currentTarget.appendChild(item)
+  e.currentTarget.classList.remove("over")
+}
+
+// cards
+cards.forEach((card) => {
+  card.addEventListener("dragover", dragOver)
+  card.addEventListener("drop", drag)
+})
