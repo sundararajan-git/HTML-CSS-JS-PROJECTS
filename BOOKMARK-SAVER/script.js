6e08a48e2d33701f93a002b3039a0cabf5400fc2
sundararajan-git <sundararajanselvarasu@gmail.com>
2025-09-02 14:35:46 +0530

    bookmark saver done

--- BOOKMARK-SAVER/script.js
+++ BOOKMARK-SAVER/script.js
@@ -3,40 +3,53 @@ const bookmarkContainer = document.getElementById("bookmarkContainerLists");
 const bookmarkFom = document.getElementById("bookmarkForm");
 
 // variable
-const bookmakList = [
+const bookmarkList = [
   {
     bookmarkName: "Google",
     bookmarkURL: "https://google.com",
+    id: 1
   },
 ];
 
 // tempaltes
 class Templates {
   getBookmarkLists(item) {
-    return `<div id="list">
-                <span>${item?.bookmarkName}</span>
-                <button type="button" onclick="removebtn()">Remove</button>
-            </div>`;
+    return `<div class="list">
+        <span><a href="${item?.bookmarkURL}" target="_blank">${item?.bookmarkName}</a></span>
+        <button type="button" class="remove-btn" onClick="removebtn(${item.id})">Remove</button>
+      </div>`;
+
   }
 }
 
 const template = new Templates();
 
-const removebtn = () => {
-  alert("removed");
+const removebtn = (id) => {
+  const confirmDelete = confirm("Are you sure you want to delete this bookmark?");
+  if (!confirmDelete) return;
+  const index = bookmarkList.findIndex((item) => item.id === id)
+  bookmarkList.splice(index, 1)
+  renderBookmarkLists()
 };
 
 const renderBookmarkLists = () => {
-  bookmakList.forEach((item) => {
-    const temp = template.getBookmarkLists(item);
-    bookmarkContainer.insertAdjacentHTML("beforeend", temp);
-  });
+  bookmarkContainer.innerHTML = ""
+  if (bookmarkList.length) {
+    bookmarkList.forEach((item) => {
+      const temp = template.getBookmarkLists(item);
+      bookmarkContainer.insertAdjacentHTML("beforeend", temp);
+    });
+  } else {
+    bookmarkContainer.insertAdjacentHTML("beforeend", "<p>No bookmarks added yet.</p>");
+  }
 };
 
 const addBookmarkHandler = (event) => {
   event.preventDefault();
   const formData = new FormData(bookmarkFom);
   const formJson = Object.fromEntries(formData);
+  formJson.id = bookmarkList.length + 1
+  bookmarkList.push(formJson)
   const temp = template.getBookmarkLists(formJson);
   bookmarkContainer.insertAdjacentHTML("beforeend", temp);
   bookmarkFom.reset();
@@ -45,3 +58,4 @@ const addBookmarkHandler = (event) => {
 // trigeers
 window.addEventListener("DOMContentLoaded", renderBookmarkLists);
 bookmarkFom.addEventListener("submit", addBookmarkHandler);
+
