554e75d5f91e53897b396785c269801542c7ed3b
sundararajan-git <sundararajan-git@users.noreply.github.com>
2025-09-03 07:42:57 +0000

    deploy: dbd7c0bf8805a2b1bced49caca62fcff20ffec0f

---
+++ BMI-CALCULATOR/script.js
@@ -0,0 +1,30 @@
+const weightInput = document.getElementById("weight");
+const heightInput = document.getElementById("height");
+const calculateBtn = document.getElementById("calculateBtn");
+const resultDiv = document.getElementById("result");
+
+calculateBtn.addEventListener("click", () => {
+    const weight = parseFloat(weightInput.value);
+    const heightCm = parseFloat(heightInput.value);
+
+    if (!weight || !heightCm) {
+        resultDiv.textContent = "Please enter valid values!";
+        resultDiv.style.color = "red";
+        return;
+    }
+
+    const heightM = heightCm / 100;
+    const bmi = weight / (heightM * heightM);
+    let category = "";
+
+    if (bmi < 18.5) category = "Underweight";
+    else if (bmi < 25) category = "Normal weight";
+    else if (bmi < 30) category = "Overweight";
+    else category = "Obese";
+
+    resultDiv.textContent = `BMI: ${bmi.toFixed(2)} (${category})`;
+
+    if (category === "Normal weight") resultDiv.style.color = "green";
+    else if (category === "Underweight") resultDiv.style.color = "orange";
+    else resultDiv.style.color = "red";
+});
