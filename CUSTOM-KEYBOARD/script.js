7769d0f43971a45aef2c5a734c1c65bfaf8e5dd1
sundararajan-git <sundararajanselvarasu@gmail.com>
2025-09-13 16:00:44 +0530

    up

--- CUSTOM-KEYBOARD/script.js
+++ CUSTOM-KEYBOARD/script.js
@@ -1,32 +1,35 @@
+
+// dom elements
+const keyboard = document.getElementById('keyboard');
+const inputField = document.getElementById('inputField');
+const backSpaceBtn = document.getElementById('backspace');
+const spaceBtn = document.getElementById('space')
+
+
+// var
 const keys = [
     'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P',
     'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L',
     'Z', 'X', 'C', 'V', 'B', 'N', 'M'
 ];
 
-const keyboard = document.getElementById('keyboard');
-const inputField = document.getElementById('inputField');
-
-// Generate keys
 keys.forEach(key => {
     const btn = document.createElement('button');
     btn.textContent = key;
-    btn.className = 'key px-3 py-2 bg-gray-200 rounded hover:bg-gray-300 transition';
+    btn.className = 'key px-2 py-2 bg-gray-200 rounded hover:bg-gray-300 transition text-center cursor-pointer';
     btn.addEventListener('click', () => {
-        inputField.value += key;
+        inputField.value += key?.toLowerCase();
         inputField.focus();
     });
     keyboard.appendChild(btn);
 });
 
-// Backspace
-document.getElementById('backspace').addEventListener('click', () => {
+backSpaceBtn.addEventListener('click', () => {
     inputField.value = inputField.value.slice(0, -1);
     inputField.focus();
 });
 
-// Space
-document.getElementById('space').addEventListener('click', () => {
+spaceBtn.addEventListener('click', () => {
     inputField.value += ' ';
     inputField.focus();
 });
