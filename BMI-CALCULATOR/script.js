5497554331d7b27ac0281537c71f94e9b5ae2b5e
sundararajan-git <sundararajanselvarasu@gmail.com>
2025-09-13 07:49:04 +0530

    up

--- BMI-CALCULATOR/script.js
+++ BMI-CALCULATOR/script.js
@@ -4,6 +4,7 @@ const calculateBtn = document.getElementById("calculateBtn");
 const resultDiv = document.getElementById("result");
 
 calculateBtn.addEventListener("click", () => {
+
     const weight = parseFloat(weightInput.value);
     const heightCm = parseFloat(heightInput.value);
 
@@ -17,14 +18,37 @@ calculateBtn.addEventListener("click", () => {
     const bmi = weight / (heightM * heightM);
     let category = "";
 
-    if (bmi < 18.5) category = "Underweight";
-    else if (bmi < 25) category = "Normal weight";
-    else if (bmi < 30) category = "Overweight";
-    else category = "Obese";
+    if (bmi < 18.5) {
+        category = "Underweight";
+    }
+    else if (bmi < 25) {
+        category = "Normal weight";
+    }
+    else if (bmi < 30) {
+        category = "Overweight";
+    }
+    else {
+        category = "Obese";
+    }
+
+    if (category === "Normal weight") {
+        resultDiv.style.color = "green";
+    }
+    else if (category === "Underweight") {
+        resultDiv.style.color = "orange";
+    }
+    else {
+        resultDiv.style.color = "red";
+    }
 
     resultDiv.textContent = `BMI: ${bmi.toFixed(2)} (${category})`;
 
-    if (category === "Normal weight") resultDiv.style.color = "green";
-    else if (category === "Underweight") resultDiv.style.color = "orange";
-    else resultDiv.style.color = "red";
 });
+
+weightInput.addEventListener('input', () => {
+    resultDiv.textContent = ``;
+})
+
+heightInput.addEventListener('input', () => {
+    resultDiv.textContent = ``;
+})
